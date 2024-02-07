66abd28a318a3dc0fb2e66769fbd00860b7964a6
Salim <salimaloui406@gmail.com>
2024-02-07 21:01:39 +0100

    Initial commit

---
+++ src/main/java/com/example/intershipmangement/IntershipMangementApplication.java
@@ -0,0 +1,13 @@
+package com.example.intershipmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipMangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntershipMangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/intershipmangement/IntershipMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intershipmangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntershipMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
