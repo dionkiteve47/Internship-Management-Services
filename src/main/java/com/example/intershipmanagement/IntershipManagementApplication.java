cc3efc375b0c09e6b0855fcc085a22a4712ecc8a
Salim <salimaloui406@gmail.com>
2024-02-15 14:23:21 +0100

    "INITIAL COMMIT"

---
+++ src/main/java/com/example/intershipmanagement/IntershipManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.intershipmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntershipManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntershipManagementApplication.class, args);
+	}
+
+}
